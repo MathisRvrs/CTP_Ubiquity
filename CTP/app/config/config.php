57ef1c773e161338c906c6687a3164433112e8fb
Mathis <mathisrouvers@gmail.com>
2022-03-24 13:59:43 +0100

    Fin intro

--- CTP/app/config/config.php
+++ CTP/app/config/config.php
@@ -1,26 +1,40 @@
 <?php
 return array(
-		"siteUrl"=>"http://127.0.0.1/CTP/public/",
-		"database"=>[
-				"type"=>"mysql",
-				"dbName"=>"",
-				"serverName"=>"127.0.0.1",
-				"port"=>"3306",
-				"user"=>"root",
-				"password"=>"",
-				"options"=>[],
-				"cache"=>false
-		],
-		"sessionName"=>"CTP",
-		"namespaces"=>[],
-		"templateEngine"=>'Ubiquity\\views\\engine\\Twig',
-		"templateEngineOptions"=>array("cache"=>false),
-		"test"=>false,
-		"debug"=>false,
-		"logger"=>function(){return new \Ubiquity\log\libraries\UMonolog("CTP",\Monolog\Logger::INFO);},
-		"di"=>["@exec"=>["jquery"=>function($controller){
+	"siteUrl"=>"http://127.0.0.1/CTP/public/",
+	"database"=>[
+			"type"=>"mysql",
+			"dbName"=>"store",
+			"serverName"=>"127.0.0.1",
+			"port"=>3306,
+			"user"=>"root",
+			"password"=>"",
+			"options"=>array(),
+			"cache"=>false,
+			"wrapper"=>"Ubiquity\\db\\providers\\pdo\\PDOWrapper"
+			],
+	"sessionName"=>"CTP",
+	"namespaces"=>array(),
+	"templateEngine"=>"Ubiquity\\views\\engine\\Twig",
+	"templateEngineOptions"=>[
+			"cache"=>false
+			],
+	"test"=>false,
+	"debug"=>false,
+	"logger"=>function (){return new \Ubiquity\log\libraries\UMonolog("CTP",\Monolog\Logger::INFO);},
+	"di"=>[
+			"@exec"=>array("jquery"=>function ($controller){
 						return \Ajax\php\ubiquity\JsUtils::diSemantic($controller);
-					}]],
-		"cache"=>["directory"=>"cache/","system"=>"Ubiquity\\cache\\system\\ArrayCache","params"=>[]],
-		"mvcNS"=>["models"=>"models","controllers"=>"controllers","rest"=>""]
-);
+					})
+			],
+	"cache"=>[
+			"directory"=>"cache/",
+			"system"=>"Ubiquity\\cache\\system\\ArrayCache",
+			"params"=>array()
+			],
+	"mvcNS"=>[
+			"models"=>"models",
+			"controllers"=>"controllers",
+			"rest"=>"",
+			"domains"=>"domains"
+			]
+	);
